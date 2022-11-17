c437f99a01feeb9cee6c0c24aa03f2e08cacf066
liguanghao <liguanghao@viatomtech.com>
2022-11-17 14:23:31 +0800

    sdf

--- app/src/main/java/com/vaca/headgraph/MainActivity.kt
+++ app/src/main/java/com/vaca/headgraph/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.vaca.headgraph
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.luck.picture.lib.basic.PictureSelector
 import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.engine.CompressFileEngine
 import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener
 import com.luck.picture.lib.interfaces.OnResultCallbackListener
+import top.zibin.luban.Luban
+import top.zibin.luban.OnNewCompressListener
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +30,42 @@ class MainActivity : AppCompatActivity() {
         PictureSelector.create(this)
             .openGallery(SelectMimeType.ofImage())
             .setImageEngine(GlideEngine.createGlideEngine())
+            .setCompressEngine(object:CompressFileEngine{
+                override fun onStartCompress(
+                    context: Context?,
+                    source: java.util.ArrayList<Uri>?,
+                    call: OnKeyValueResultCallbackListener?
+                ) {
+                    Luban.with(this@MainActivity).load(source).ignoreBy(100).setCompressListener(object:OnNewCompressListener{
+                        override fun onStart() {
+
+                        }
+
+                        override fun onSuccess(source: String?, compressFile: File?) {
+                            call?.onCallback(source,compressFile?.absolutePath)
+                        }
+
+                        override fun onError(source: String?, e: Throwable?) {
+                            call?.onCallback(source,null)
+                        }
+
+                    }).launch()
+
+                }
+
+            })
             .forResult(object : OnResultCallbackListener<LocalMedia?> {
-                override fun onResult(result: ArrayList<LocalMedia?>?) {}
+                override fun onResult(result: ArrayList<LocalMedia?>?) {
+                    Log.e("fuck",result?.size.toString())
+                    result?.let {
+                        for(i in it){
+                            val path=i!!.compressPath
+                            Log.e("fuckyout",path)
+                        }
+                    }
+
+
+                }
                 override fun onCancel() {}
             })
 
