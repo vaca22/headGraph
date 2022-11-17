123f6d46ba85a8c13e374472da6edb980dca70dd
liguanghao <liguanghao@viatomtech.com>
2022-11-17 14:31:56 +0800

    sdf54654

--- app/src/main/java/com/vaca/headgraph/MainActivity.kt
+++ app/src/main/java/com/vaca/headgraph/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.vaca.headgraph
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.luck.picture.lib.basic.PictureSelector
 import com.luck.picture.lib.config.SelectMimeType
 import com.luck.picture.lib.engine.CompressFileEngine
+import com.luck.picture.lib.engine.CropEngine
+import com.luck.picture.lib.engine.CropFileEngine
 import com.luck.picture.lib.entity.LocalMedia
 import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener
 import com.luck.picture.lib.interfaces.OnResultCallbackListener
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.UCropImageEngine
 import top.zibin.luban.Luban
 import top.zibin.luban.OnNewCompressListener
 import java.io.File
@@ -54,6 +62,40 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
+            .setCropEngine(object :CropFileEngine{
+                override fun onStartCrop(
+                    fragment: Fragment?,
+                    srcUri: Uri?,
+                    destinationUri: Uri?,
+                    dataSource: java.util.ArrayList<String>?,
+                    requestCode: Int
+                ) {
+                   val  uCrop = UCrop.of(srcUri!!, destinationUri!!, dataSource);
+                    uCrop.setImageEngine(object:UCropImageEngine{
+                        override fun loadImage(
+                            context: Context?,
+                            url: String?,
+                            imageView: ImageView?
+                        ) {
+                          Glide.with(this@MainActivity).load(url).into(imageView!!)
+                        }
+
+                        override fun loadImage(
+                            context: Context?,
+                            url: Uri?,
+                            maxWidth: Int,
+                            maxHeight: Int,
+                            call: UCropImageEngine.OnCallbackListener<Bitmap>?
+                        ) {
+
+                        }
+
+                    })
+                    uCrop.start(fragment!!.requireActivity(),fragment,requestCode)
+                }
+
+            })
+
             .forResult(object : OnResultCallbackListener<LocalMedia?> {
                 override fun onResult(result: ArrayList<LocalMedia?>?) {
                     Log.e("fuck",result?.size.toString())
